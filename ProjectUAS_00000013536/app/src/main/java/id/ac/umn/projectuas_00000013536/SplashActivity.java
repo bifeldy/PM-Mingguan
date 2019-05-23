b596bc340ca080bf7399f9aa35d0a135207a6fa0
Bifeldy <bias.astho@gmail.com>
2019-05-23 09:14:57 +0700

    アップデート

---
+++ ProjectUAS_00000013536/app/src/main/java/id/ac/umn/projectuas_00000013536/SplashActivity.java
@@ -0,0 +1,13 @@
+package id.ac.umn.projectuas_00000013536;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
