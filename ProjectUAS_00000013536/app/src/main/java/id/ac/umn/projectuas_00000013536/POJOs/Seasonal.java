89360423966b4fb570f8634a03a6a821fa07ccb1
Bifeldy <bias.astho@gmail.com>
2019-05-26 02:17:10 +0700

    Added Dummy Data ^_^.~

---
+++ ProjectUAS_00000013536/app/src/main/java/id/ac/umn/projectuas_00000013536/POJOs/Seasonal.java
@@ -0,0 +1,70 @@
+package id.ac.umn.projectuas_00000013536.POJOs;
+
+import java.util.List;
+
+public class Seasonal {
+
+    private String request_hash;
+    private boolean request_cached;
+    private int request_cache_expiry;
+    private String season_name;
+    private int season_year;
+    private List<SeasonalAnime> anime;
+
+    public Seasonal(String request_hash, boolean request_cached, int request_cache_expiry, String season_name, int season_year, List<SeasonalAnime> anime) {
+        this.request_hash = request_hash;
+        this.request_cached = request_cached;
+        this.request_cache_expiry = request_cache_expiry;
+        this.season_name = season_name;
+        this.season_year = season_year;
+        this.anime = anime;
+    }
+
+    public String getRequest_hash() {
+        return request_hash;
+    }
+
+    public boolean isRequest_cached() {
+        return request_cached;
+    }
+
+    public int getRequest_cache_expiry() {
+        return request_cache_expiry;
+    }
+
+    public String getSeason_name() {
+        return season_name;
+    }
+
+    public int getSeason_year() {
+        return season_year;
+    }
+
+    public List<SeasonalAnime> getAnime() {
+        return anime;
+    }
+
+    public void setRequest_hash(String request_hash) {
+        this.request_hash = request_hash;
+    }
+
+    public void setRequest_cached(boolean request_cached) {
+        this.request_cached = request_cached;
+    }
+
+    public void setRequest_cache_expiry(int request_cache_expiry) {
+        this.request_cache_expiry = request_cache_expiry;
+    }
+
+    public void setSeason_name(String season_name) {
+        this.season_name = season_name;
+    }
+
+    public void setSeason_year(int season_year) {
+        this.season_year = season_year;
+    }
+
+    public void setAnime(List<SeasonalAnime> anime) {
+        this.anime = anime;
+    }
+}
---
+++ ProjectUAS_00000013536/app/src/main/java/id/ac/umn/projectuas_00000013536/POJOs/SeasonalAnime.java
@@ -0,0 +1,180 @@
+package id.ac.umn.projectuas_00000013536.POJOs;
+
+import java.util.List;
+
+public class SeasonalAnime {
+
+    private int mal_id;
+    private String url;
+    private String title;
+    private String image_url;
+    private String synopsis;
+    private String type;
+    private String airing_start;
+    private int episodes;
+    private int members;
+    private List<Genre> genres;
+    private String source;
+    private List<Producer> producers;
+    private double score;
+    private List<String> licensors;
+    private boolean r18;
+    private boolean kids;
+    private boolean continuing;
+
+    public SeasonalAnime(int mal_id, String url, String title, String image_url, String synopsis, String type, String airing_start, int episodes, int members, List<Genre> genres, String source, List<Producer> producers, double score, List<String> licensors, boolean r18, boolean kids, boolean continuing) {
+        this.mal_id = mal_id;
+        this.url = url;
+        this.title = title;
+        this.image_url = image_url;
+        this.synopsis = synopsis;
+        this.type = type;
+        this.airing_start = airing_start;
+        this.episodes = episodes;
+        this.members = members;
+        this.genres = genres;
+        this.source = source;
+        this.producers = producers;
+        this.score = score;
+        this.licensors = licensors;
+        this.r18 = r18;
+        this.kids = kids;
+        this.continuing = continuing;
+    }
+
+    public int getMal_id() {
+        return mal_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getAiring_start() {
+        return airing_start;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public List<Producer> getProducers() {
+        return producers;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public List<String> getLicensors() {
+        return licensors;
+    }
+
+    public boolean isR18() {
+        return r18;
+    }
+
+    public boolean isKids() {
+        return kids;
+    }
+
+    public boolean isContinuing() {
+        return continuing;
+    }
+
+    public void setMal_id(int mal_id) {
+        this.mal_id = mal_id;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAiring_start(String airing_start) {
+        this.airing_start = airing_start;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setProducers(List<Producer> producers) {
+        this.producers = producers;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public void setLicensors(List<String> licensors) {
+        this.licensors = licensors;
+    }
+
+    public void setR18(boolean r18) {
+        this.r18 = r18;
+    }
+
+    public void setKids(boolean kids) {
+        this.kids = kids;
+    }
+
+    public void setContinuing(boolean continuing) {
+        this.continuing = continuing;
+    }
+}
